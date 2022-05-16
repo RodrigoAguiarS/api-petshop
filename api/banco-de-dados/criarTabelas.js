5894cf4830c16e7806b08eee62bda9aacc839e17
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2022-05-15 22:03:01 -0300

    criando tabelas de produtos

--- api/banco-de-dados/criarTabelas.js
+++ api/banco-de-dados/criarTabelas.js
@@ -1,6 +1,6 @@
 const modelos = [
     require('../rotas/fornecedores/ModeloTabelaFornecedor'),
-    require('../rotas/fornecedores/produtos/modeloTabelaProduto')
+    require('../rotas/fornecedores/produtos/ModeloTabelaProduto')
 ]
 
 async function criarTabelas () {
